d7080d7d179615955fc115057900676c387d4473
Alex Ivanov <ivanov.alex1@gmail.com>
2019-02-01 12:24:07 +0200

    add to localStorage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 
 let taskList = [];
 
+let outTask = () => {
+    let li = document.createElement('LI');
+    let index = taskList.length;
+    let {todo, checked} = {...taskList[index-1]};
+    let textNode = document.createTextNode(todo);
+    li.appendChild(textNode);
+    document.getElementById('taskList').appendChild(li);
+    document.getElementById('myInput').value = "";
+}
+
+// if(localStorage.getItem('todo') != undefined){
+//    taskList = JSON.parse(localStorage.getItem('todo'))
+//    for(let key = 0; key < taskList.length; key++){
+//        let task = {};
+//        let {todo, checked} = {...taskList[key]};
+//        outTask();
+//        console.log(todo);
+//        console.log(checked);
+//    }
+// }
+
 let addNewTask = () => {
     let newTask = document.getElementById('myInput').value;
     let task = {};
@@ -10,19 +31,9 @@ let addNewTask = () => {
         task.todo = newTask;
         task.checked = false;
         taskList.push(task);
+        localStorage.setItem('todo', JSON.stringify(taskList));
         outTask();
     }
-}
-
-let outTask = () => {
-    let li = document.createElement('LI');
-    let index = taskList.length;
-    let {todo, checked} = {...taskList[index-1]};
-    let textNode = document.createTextNode(todo);
-    li.appendChild(textNode);
-    document.getElementById('taskList').appendChild(li);
-    document.getElementById('myInput').value = "";
-
 }
 
 let list = document.querySelector('UL');
