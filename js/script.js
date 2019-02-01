9391a53e46f5e76059cf5929384887266b172deb
Alex Ivanov <ivanov.alex1@gmail.com>
2019-02-01 12:01:52 +0200

    Add new task, done task and out on screen

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+let taskList = [];
+
+let addNewTask = () => {
+    let newTask = document.getElementById('myInput').value;
+    let task = {};
+    if(newTask === ""){
+        alert ('Input task');
+    }else {
+        task.todo = newTask;
+        task.checked = false;
+        taskList.push(task);
+        outTask();
+    }
+}
+
+let outTask = () => {
+    let li = document.createElement('LI');
+    let index = taskList.length;
+    let {todo, checked} = {...taskList[index-1]};
+    let textNode = document.createTextNode(todo);
+    li.appendChild(textNode);
+    document.getElementById('taskList').appendChild(li);
+    document.getElementById('myInput').value = "";
+
+}
+
+let list = document.querySelector('UL');
+list.addEventListener('click', function(event) {
+    if(event.target.tagName === 'LI'){
+        event.target.classList.toggle('checked');
+    }
+}, false);
+
+
+// let deleteTask = () => {
+//     document.getElementsByClassName
+// }
+    
+
